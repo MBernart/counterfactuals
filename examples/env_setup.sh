8b0e74d4696daa1c7d01dcd68879cd51b75dd246
Mateusz Bernart <mateuszbernart@gmail.com>
2025-10-24 16:59:00 +0200

    env_setup helper

---
+++ examples/env_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker run --rm -d -p 6379:6379 --name celery-redis redis
+
+# celery -A explainers.wachter.main worker -l info -n wachter_worker@%h -Q wachter,celery
+# celery -A explainers.growing_spheres.main worker -l info -n growing_spheres_worker@%h -Q growing_spheres,celery
+
+docker run --rm -d --network host --name counterfactuals-alibi-cfproto cfe.cs.put.poznan.pl/counterfactuals-alibi cfproto
+docker run --rm -d --network host --name counterfactuals-alibi-cfrl cfe.cs.put.poznan.pl/counterfactuals-alibi cfrl
+
+docker run --rm -d --network host --name counterfactuals-carla-actionable_recourse cfe.cs.put.poznan.pl/counterfactuals-carla actionable_recourse
+docker run --rm -d --network host --name counterfactuals-carla-dice cfe.cs.put.poznan.pl/counterfactuals-carla dice
+docker run --rm -d --network host --name counterfactuals-carla-face cfe.cs.put.poznan.pl/counterfactuals-carla face
