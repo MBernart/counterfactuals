68381956816e516adeb29b9c343f97742f1ade19
Patryk Janiak <xmorswinx@gmail.com>
2025-11-13 21:48:52 +0100

    handle categorical features in cfrl

--- experiments/titanic.py
+++ experiments/titanic.py
@@ -1,23 +1,16 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
-# from explainers_lib.explainers.wachter import WachterExplainer
-# from explainers_lib.explainers.growing_spheres import GrowingSpheresExplainer
-# from explainers_lib.explainers.celery_explainer import ActionableRecourseExplainer
-# from explainers_lib.explainers.celery_explainer import FaceExplainer
-# from explainers_lib.explainers.celery_explainer import DiceExplainer
-# from explainers_lib.explainers.celery_explainer import AlibiCFProto
-# from explainers_lib.explainers.celery_explainer import AlibiCFRL
-# from explainers_lib.explainers.celery_explainer import AlibiCFProto
-# from explainers_lib.explainers.celery_explainer import AlibiCFRL
+from explainers_lib.explainers.wachter import WachterExplainer
+from explainers_lib.explainers.growing_spheres import GrowingSpheresExplainer
+from explainers_lib.explainers.celery_explainer import ActionableRecourseExplainer
+from explainers_lib.explainers.celery_explainer import FaceExplainer
+from explainers_lib.explainers.celery_explainer import DiceExplainer
+from explainers_lib.explainers.celery_explainer import AlibiCFProto
+from explainers_lib.explainers.celery_explainer import AlibiCFRL
 from explainers_lib.aggregators import Pareto, All
 from explainers_lib.datasets import Dataset
 from explainers_lib.ensemble import Ensemble
 from explainers_lib.model import TorchModel
-
-import sys
-sys.path.append("..")
-
-from explainers.alibi.cfproto import CFProto as AlibiCFProto
 
 
 url = "https://web.stanford.edu/class/archive/cs/cs109/cs109.1166/stuff/titanic.csv"
@@ -44,10 +37,11 @@ with open("models/titanic_classifier.pt", "rb") as f:
 model = TorchModel.deserialize(model_data)
 print(model, input_dim)
 
-ensemble = Ensemble(model, [
-    AlibiCFProto(),
-    # AlibiCFRL()
-], All())
+ensemble = Ensemble(model, 
+    [WachterExplainer(), GrowingSpheresExplainer(),                   # Local explainers
+     ActionableRecourseExplainer(), DiceExplainer(), FaceExplainer(), # Carla explainers
+     AlibiCFProto(), AlibiCFRL()],                                    # Alibi explainers
+    All())
 ensemble.fit(ds)
 cfs = ensemble.explain(ds[:5], pretty_print=True, pretty_print_postprocess=ds.inverse_transform, feature_names=ds.features)
 cfs
