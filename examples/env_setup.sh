4f05ee52da2980a3cf1d357f92fde96d1caec603
Patryk Janiak <xmorswinx@gmail.com>
2025-11-15 12:22:50 +0100

    update env_setup script

--- examples/env_setup.sh
+++ examples/env_setup.sh
@@ -3,17 +3,20 @@
 set -xe
 
 docker pull redis:8.2.3
+docker pull cfe.cs.put.poznan.pl/counterfactuals-native
 docker pull cfe.cs.put.poznan.pl/counterfactuals-alibi
 docker pull cfe.cs.put.poznan.pl/counterfactuals-carla
+docker pull cfe.cs.put.poznan.pl/counterfactuals-dice
 
 docker run --rm -d -p 6379:6379 --name celery-redis redis:8.2.3
 
-# celery -A explainers.wachter.main worker -l info -n wachter_worker@%h -Q wachter,celery
-# celery -A explainers.growing_spheres.main worker -l info -n growing_spheres_worker@%h -Q growing_spheres,celery
+# docker run --rm -d --network host --name counterfactuals-wachter cfe.cs.put.poznan.pl/counterfactuals-native wachter
+# docker run --rm -d --network host --name counterfactuals-growing_spheres cfe.cs.put.poznan.pl/counterfactuals-native growing_spheres
+docker run --rm -d --network host --name counterfactuals-face cfe.cs.put.poznan.pl/counterfactuals-native face
 
 docker run --rm -d --network host --name counterfactuals-alibi-cfproto cfe.cs.put.poznan.pl/counterfactuals-alibi cfproto
 docker run --rm -d --network host --name counterfactuals-alibi-cfrl cfe.cs.put.poznan.pl/counterfactuals-alibi cfrl
 
 docker run --rm -d --network host --name counterfactuals-carla-actionable_recourse cfe.cs.put.poznan.pl/counterfactuals-carla actionable_recourse
-docker run --rm -d --network host --name counterfactuals-carla-dice cfe.cs.put.poznan.pl/counterfactuals-carla dice
-docker run --rm -d --network host --name counterfactuals-carla-face cfe.cs.put.poznan.pl/counterfactuals-carla face
+
+docker run --rm -d --network host --name counterfactuals-dice cfe.cs.put.poznan.pl/counterfactuals-dice dice
