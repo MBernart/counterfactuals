c29c2f69a705316aa9525be4f5a3ac346e32528e
Mateusz Bernart <mateuszbernart@gmail.com>
2025-11-30 20:39:13 +0100

    correct model type check

--- src/explainers_lib/explainers/native/wachter.py
+++ src/explainers_lib/explainers/native/wachter.py
@@ -102,7 +102,7 @@ class WachterExplainer(Explainer):
             self.feature_bounds.append((0.0, 1.0))
 
     def explain(self, model: Model, data: Dataset, y_desired: int = None) -> List[Counterfactual]:
-        if isinstance(data, TorchModel):
+        if isinstance(model, TorchModel):
             model._model.to("cuda")
 
         counterfactuals = []
