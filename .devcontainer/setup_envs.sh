1a416f4b8aa11ca9a128322d3bdcc812fa18f403
Mateusz Bernart <mateuszbernart@gmail.com>
2026-03-02 17:49:56 +0100

    Native only

--- .devcontainer/setup_envs.sh
+++ .devcontainer/setup_envs.sh
@@ -3,21 +3,10 @@
 # Exit on error
 set -e
 
-echo "Initializing .venv (Main Library)..."
+echo "Initializing .venv (Natives Only)..."
 python3 -m venv .venv
 .venv/bin/pip install --upgrade pip
 .venv/bin/pip install -e ./src
+.venv/bin/pip install -r explainers/native/requirements.minimal.txt
 
-echo "Initializing .venv-alibi (Alibi Explainer)..."
-python3 -m venv .venv-alibi
-.venv-alibi/bin/pip install --upgrade pip
-.venv-alibi/bin/pip install -e ./src
-.venv-alibi/bin/pip install -r explainers/alibi/requirements.minimal.txt
-
-echo "Initializing .venv-dice (DiCE Explainer)..."
-python3 -m venv .venv-dice
-.venv-dice/bin/pip install --upgrade pip
-.venv-dice/bin/pip install -e ./src
-.venv-dice/bin/pip install -r explainers/dice/requirements.minimal.txt
-
-echo "All environments initialized successfully."
+echo "Environment initialized successfully."
